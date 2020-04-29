2e890a40a0a95293a07c03b573cd7cb47dbe83b8
Judit <juditgoes@gmail.com>
2020-04-29 19:20:43 +0100

    initial commit

---
+++ alim.js
@@ -0,0 +1,17 @@
+function scrus(){
+var elmnt = document.getElementById("us");
+elmnt.scrollIntoView({behavior: "smooth",block:"center"});
+}
+function scrga(){
+var elmnt = document.getElementById("ga");
+elmnt.scrollIntoView({behavior: "smooth",block:"center"});
+}
+function scrco(){
+var elmnt = document.getElementById("co");
+elmnt.scrollIntoView({behavior: "smooth",block:"start"});
+}
+function totop(){
+var elmnt = document.getElementById("atop");
+elmnt.scrollIntoView({behavior: "smooth",block:"start"});
+}
+
